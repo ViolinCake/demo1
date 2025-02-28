a1c43d6e58913d7a7fd973adf4b1c9b19b8c65ef
ViolinCake <110251345+ViolinCake@users.noreply.github.com>
2025-02-28 14:43:44 +0200

    Update PersonService.java testjenkins1

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,12 +40,7 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 
-		p = new Person();
-		p.setId("5");
-		p.setAge(33);
-		p.setFirstName("webhook");
-		p.setLastName("testing");
-		persons.add(p);
+
 
 
 		
